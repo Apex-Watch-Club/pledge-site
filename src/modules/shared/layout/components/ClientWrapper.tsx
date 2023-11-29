34a91271445cb0413758af21839c42e7c1699a4b
Kristian Quirapas <kmquirapas@gmail.com>
2023-11-29 17:28:30 +0800

    fix; removed localhost in wagmi config

--- src/modules/shared/layout/components/ClientWrapper.tsx
+++ src/modules/shared/layout/components/ClientWrapper.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { ReactNode } from "react";
 import { WagmiConfig, createConfig, configureChains } from "wagmi";
-import { localhost, mainnet } from "wagmi/chains";
+import { localhost, goerli, mainnet } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { ToasterProvider } from "@/modules/shared/toaster";
@@ -10,7 +10,7 @@ const LOCALHOST_RPC_URL = "http://localhost:8545";
 const ALCHEMY_API_KEY = "iqutKCtSJcHpdW9WQVc2Zn08iK8yP9wz";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [localhost, mainnet],
+  [localhost, goerli, mainnet],
   [
     jsonRpcProvider({ rpc: (chain) => ({ http: LOCALHOST_RPC_URL }) }),
     alchemyProvider({ apiKey: ALCHEMY_API_KEY }),
