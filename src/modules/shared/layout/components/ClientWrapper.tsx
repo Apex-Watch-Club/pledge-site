6c503a148acf61a8a38a9a3094efa08e624fce15
Kristian Quirapas <kmquirapas@gmail.com>
2023-11-29 17:51:16 +0800

    fix: add jsonRPCProvider localhost

--- src/modules/shared/layout/components/ClientWrapper.tsx
+++ src/modules/shared/layout/components/ClientWrapper.tsx
@@ -6,13 +6,13 @@ import { alchemyProvider } from "wagmi/providers/alchemy";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { ToasterProvider } from "@/modules/shared/toaster";
 
-// const LOCALHOST_RPC_URL = "http://localhost:8545";
+const LOCALHOST_RPC_URL = "http://localhost:8545";
 const ALCHEMY_API_KEY = "iqutKCtSJcHpdW9WQVc2Zn08iK8yP9wz";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [localhost, goerli, mainnet],
   [
-    // jsonRpcProvider({ rpc: (chain) => ({ http: LOCALHOST_RPC_URL }) }),
+    jsonRpcProvider({ rpc: (chain) => ({ http: LOCALHOST_RPC_URL }) }),
     alchemyProvider({ apiKey: ALCHEMY_API_KEY }),
   ],
 );
