70fcf9689933078522ee281418a1bebb963106c0
Kristian Quirapas <kmquirapas@gmail.com>
2023-12-12 10:05:28 +0800

    add: OG metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Apex Watch Club",
-  description: "Apex Watch Club",
+  description: "The Future of Luxury Watch Ownership and Trading",
+  openGraph: {
+    title: "Apex Watch Club",
+    description: "The Future of Luxury Watch Ownership and Trading",
+    url: "https://www.apexwatchclub.com/",
+    images:
+      "https://bafybeiayltj5dzslzd2fohtl2sxdrublo53rzxmwy7stth5wulzroisimu.ipfs.nftstorage.link/",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Apex Watch Club",
+    description: "The Future of Luxury Watch Ownership and Trading",
+    creator: "Apex Watch Club",
+    images: [
+      "https://bafybeiayltj5dzslzd2fohtl2sxdrublo53rzxmwy7stth5wulzroisimu.ipfs.nftstorage.link/",
+    ],
+  },
 };
 
 export default function RootLayout({
